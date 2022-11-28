77b2081c8d07c1ce1dc7c78d12e2effb75018a80
melvinoh <murichumelvin@gmail.com>
2022-11-28 19:07:39 +0300

    ecommerce xtatic shoez app

---
+++ app/src/main/java/com/shoe/xtatic/Social_activity.kt
@@ -0,0 +1,43 @@
+package com.shoe.xtatic
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class SocialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_social)
+
+        val bottomNav : BottomNavigationView = findViewById(R.id.bottom_nav)
+
+
+        bottomNav.setOnNavigationItemSelectedListener{
+            when (it.itemId) {
+                R.id.home -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    true
+                }
+                R.id.market -> {
+                    startActivity(Intent(this, MarketActivity::class.java))
+                    true
+                }
+                R.id.social -> {
+                    startActivity(Intent(this, SocialActivity::class.java))
+                    true
+                }
+                R.id.trends -> {
+                    startActivity(Intent(this, TrendsActivity::class.java))
+                    true
+                }
+                R.id.shops-> {
+                    startActivity(Intent(this, ShopsActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
